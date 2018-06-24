809b6cdc81381a3e589507bbe6ce9eb0014810c3
Talha-T <talha6851@hotmail.com>
2018-06-24 19:14:48 +0300

    added index and npm start script

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Bot is up!" });
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Listening to port 3000"));
